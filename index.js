ef73aee85cf5aac17a9ad8470ffbd8f50f4991e3
Asmaa01-hub <husseinasmahan89@gmail.com>
2024-06-28 11:40:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+var num1 = 1;
+var num2 = 62;
+let multiply = num1*num2;
+console.log("Multiplication result:", multiply)
+
+var random = Math.floor(Math.random()*4)+3;
+console.log("random integer greater than 0:", random);
+
+var num3 = 13;
+var num4 = 9;
+let mod = num3%num4;
+console.log("Division of num 3 by num 4:", mod);
+
+let max = Math.max(11,12,20);
+console.log("Highest nuber in the set:", max);
+
+
+
+
+
+
+
